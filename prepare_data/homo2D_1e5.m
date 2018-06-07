d4bd734e949648d88e61d1ad87204146b1866f04
Leiming Yu <leimingyu830@gmail.com>
2018-06-06 22:13:58 -0400

    update

--- prepare_data/homo2D_1e5.m
+++ prepare_data/homo2D_1e5.m
@@ -57,22 +57,50 @@ for i = 1:length(x_linspace)
       
       for k = 1:length(refractive_idx)
           refract = refractive_idx(k);
-          fprintf('%.2f %.2f %.2f %d\n', xpos, ypos, refract, testID);
+          %fprintf('%d %d %.2f %d\n', xpos, ypos, refract, testID);
           
           fname_save = sprintf('%s/test%d.mat', dir_phn, testID);
           %disp(fname);
           
-          %
-          % run simulation
-          %
-          homo2D_gen(pho_cnt, fname_save, rand_seed(testID), volume, xpos, ypos, refract)
+          %--------------------
+          %  start simulation
+          %--------------------
+          %homo2D_gen(pho_cnt, fname_save, rand_seed(testID), volume, xpos, ypos, refract)
           
+          clear cfg
+          cfg.nphoton=pho_cnt;
+          cfg.vol= volume;
+          
+          %cfg.srcpos=[1 1 1];  % change the last two pos [z, y, x]
+          srcpos_str = strcat("cfg.srcpos=[1 ", string(ypos), " ", string(xpos), "];");
+          %disp(srcpos_str);
+          %eval('cfg.srcpos=[1 1 1];')
+          eval(srcpos_str);
+          
+          cfg.srcdir=[0 0 1];
+          cfg.gpuid=1;
+          cfg.autopilot=1;
+          %
+          % configure optical properties here 
+          %
+          % cfg.prop=[0 0 1 1;0.005 1 0 1.37]; % scattering mus, anisotropy, mua, refractive index
+          cfg.prop=[0 0 1 1;0.005 1 0 refract];
+          cfg.tstart=0;
+          cfg.tend=5e-8;
+          cfg.tstep=5e-8;
+          cfg.seed = rand_seed(testID); % each random seed will have different pattern 
+
+          % calculate the flux distribution with the given config
+          [flux,~]=mcxlab(cfg);
+
+          currentImage = squeeze(sum(flux.data,4));
+          feval('save', fname_save, 'currentImage');
           
           testID = testID + 1;
           
-          break
+          %break
       end
-      break
+      %break
    end
-   break
+   %break
 end
