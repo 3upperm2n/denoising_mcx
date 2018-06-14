24e97c48fc9aa796a4d75fd5895c510fe975331a
Leiming Yu <leimingyu830@gmail.com>
2018-06-14 09:59:20 -0400

    plot

---
+++ models/dncnn_unet_rand2d/plot_1e5model.m
@@ -0,0 +1,81 @@
+clear all;
+close all;
+
+
+caxis = [-3 7];
+
+%% test 1
+%noisy input
+load('../../data/rand2d/1e+05/test1.mat');
+img_noisy = currentImage;
+figure,imagesc(log10(img_noisy),caxis);
+cb = colorbar('northoutside');
+xlabel('mm')
+ylabel('1e5')
+
+
+% model
+load('./test_results/1e5model-test1.mat');
+
+% revert log(x + 1) = y  => x = exp(y) - 1
+x = exp(output_clean) - 1;
+
+max(max(x))
+min(min(x))
+
+pos = x < 0.0;
+x(pos) = 1e-8;
+
+figure,imagesc(log10(x),caxis);
+cb = colorbar('northoutside');
+xlabel('mm')
+ylabel('1e5-NN')
+
+% clean 
+load('../../data/rand2d/1e+08/test1.mat');
+
+img_clean = currentImage;
+
+figure,imagesc(log10(img_clean),caxis);
+cb = colorbar('northoutside');
+xlabel('mm')
+ylabel('1e8')
+
+
+%% test 50 
+%noisy input
+load('../../data/rand2d/1e+05/test50.mat');
+img_noisy = currentImage;
+figure,imagesc(log10(img_noisy),caxis);
+cb = colorbar('northoutside');
+xlabel('mm')
+ylabel('1e5')
+
+
+% model
+load('./test_results/1e5model-test50.mat');
+
+% revert log(x + 1) = y  => x = exp(y) - 1
+x = exp(output_clean) - 1;
+
+max(max(x))
+min(min(x))
+
+pos = x < 0.0;
+x(pos) = 1e-8;
+
+figure,imagesc(log10(x),caxis);
+cb = colorbar('northoutside');
+xlabel('mm')
+ylabel('1e5-NN')
+
+% clean 
+load('../../data/rand2d/1e+08/test50.mat');
+
+img_clean = currentImage;
+
+figure,imagesc(log10(img_clean),caxis);
+cb = colorbar('northoutside');
+xlabel('mm')
+ylabel('1e8')
+
